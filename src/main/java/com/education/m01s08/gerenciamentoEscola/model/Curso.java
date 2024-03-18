1725c7cea572cc8e52558ee765a2b2deeb0b3e69
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-03-18 13:29:58 -0300

    Exercicio 03

---
+++ src/main/java/com/education/m01s08/gerenciamentoEscola/model/Curso.java
@@ -0,0 +1,28 @@
+package com.education.m01s08.gerenciamentoEscola.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Curso {
+
+    private static Integer proximoId = 1;
+    @Setter(AccessLevel.NONE) private Integer id;
+    private String nome;
+    private String descricao;
+    private Integer cargaHoraria;
+    @Getter private static List<Curso> listaDeCursos = new ArrayList<>();
+
+    public static Curso inserir(Curso curso){
+        curso.id = proximoId++;
+        listaDeCursos.add(curso);
+        return curso;
+    }
+
+
+}
