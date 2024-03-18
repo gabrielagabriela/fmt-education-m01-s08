f899d4c5189788173182c6fdae4c40e5e44e12ac
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-03-18 10:30:05 -0300

    Exercicio 01

---
+++ src/main/java/com/education/m01s08/gerenciamentoEscola/GerenciamentoEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.education.m01s08.gerenciamentoEscola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/education/m01s08/gerenciamentoEscola/GerenciamentoEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.m01s08.gerenciamentoEscola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
